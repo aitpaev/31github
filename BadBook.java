3780eb3192173d85cb1eba02d36465c00238de89
aitpaev <116439967+aitpaev@users.noreply.github.com>
2023-11-28 23:27:41 +0600

    Add files via upload

---
+++ BadBook.java
@@ -0,0 +1,44 @@
+public public class BadBook {
+    //...
+
+    void printTextToConsole(){
+        // our code for formatting and printing the text
+    }
+
+    
+}
+public class BookPrinter {
+
+    // methods for outputting text
+    void printTextToConsole(String text){
+        //our code for formatting and printing the text
+    }
+
+    void printTextToAnotherMedium(String text){
+        // code for writing to any other location..
+    }
+}}
+}
+public class Guitar {
+
+    private String make;
+    private String model;
+    private int volume;
+
+    //Constructors, getters & setters
+}public class MotorCar implements Car {
+
+    private Engine engine;
+
+    //Constructors, getters + setters
+
+    public void turnOnEngine() {
+        //turn on the engine!
+        engine.on();
+    }
+
+    public void accelerate() {
+        //move forward!
+        engine.powerOn(1000);
+    }
+}
